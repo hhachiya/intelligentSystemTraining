ba94ecb352dd6826578c00fc497a801f91d229aa
Hirotaka Hachiya <hacchan@gmail.com>
2017-12-12 12:59:23 +0900

    add regression_template.py

---
+++ regression_template.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import regressionData as rg
+import time
+import pdb
+
+#-------------------
+# クラスの定義始まり
+class linearRegression():
+	#------------------------------------
+	# 1) 学習データおよびモデルパラメータの初期化
+	# x: 学習入力データ（入力ベクトルの次元数×データ数のnumpy.array）
+	# y: 学習出力データ（データ数のnumpy.array）
+	def __init__(self, x, y):
+		# 学習データの設定
+		self.x = x
+		self.y = y
+		self.xDim = x.shape[0]
+		self.dNum = x.shape[1]
+	#------------------------------------
+
+	#------------------------------------
+	# 2) 最小二乗法を用いてモデルパラメータを最適化
+	# （分母の計算にFor文を用いた場合）
+	def train(self):
+		self.w = np.zeros([self.xDim,1])
+	#------------------------------------
+
+	#------------------------------------
+	# 2) 最小二乗法を用いてモデルパラメータを最適化
+	# （分母の計算に行列演算を用いた場合）
+	def trainMat(self):
+		self.w = np.zeros([self.xDim,1])
+	#------------------------------------
+	
+	#------------------------------------
+	# 3) 予測
+	# x: 入力データ（入力次元 x データ数）
+	def predict(self,x):		
+		y = []
+		return y
+	#------------------------------------
+
+	#------------------------------------
+	# 4) 二乗損失の計算
+	# x: 入力データ（入力次元 x データ数）
+	# y: 出力データ（データ数）
+	def loss(self,x,y):
+		loss = 0.0
+		return loss
+	#------------------------------------
+
+#-------------------
+# メインの始まり
+if __name__ == "__main__":
+	
+	# 1) 学習入力次元が2の場合のデーター生成
+	myData = rg.artificial(200,100, dataType="1D")
+	
+	# 2) 線形回帰モデル
+	regression = linearRegression(myData.xTrain,myData.yTrain)
+	
+	# 3) 学習（For文版）
+	sTime = time.time()
+	regression.train()
+	eTime = time.time()
+	print("train with for-loop: time={0:.4} sec".format(eTime-sTime))
+	
+	# 4) 学習（行列版）
+	sTime = time.time()
+	regression.trainMat()
+	eTime = time.time()
+	print("train with matrix: time={0:.4} sec".format(eTime-sTime))
+
+	# 5) 学習したモデルを用いて予測
+	print("loss={0:.3}".format(regression.loss(myData.xTest,myData.yTest)))
+
+	# 6) 学習・評価データおよび予測結果をプロット
+	predict = regression.predict(myData.xTest)
+	myData.plot(predict)
+	
+#-------------------
+	
